7dae0fe40c24d95d484a507002c356285eff236a
Muhammad Hafizh Fadhlurrahman <muhammad.fadhlurrahman@nexsoft.co.id>
2023-10-03 13:52:04 +0700

    initiate project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-[#CF0000]'>CMS</p>
+    </div>
+  );
+}
+
+export default App;
